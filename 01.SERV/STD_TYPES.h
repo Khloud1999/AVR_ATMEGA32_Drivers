d2c6707c2f8ce06f8524e81f26a80fd942ed4dc2
Khloud khaled Atia <72269263+Khloud1999@users.noreply.github.com>
2023-01-29 23:28:06 +0200

    Add files via upload

---
+++ 01.SERV/STD_TYPES.h
@@ -0,0 +1,33 @@
+/****************************************************/
+/* Auther : Khloud Khaled                           */
+/* Date   : 25 OCT 2022                              */
+/* File   : STD_TYPES                               */
+/* Var    : V 1.0                                   */
+/* LOG    :                                         */
+/* TO BE  :                                         */
+/****************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+	typedef unsigned char        uint8;
+	typedef signed char          sint8;
+	
+	typedef unsigned short int   uint16;
+	typedef signed short int     sint16;
+	
+	typedef unsigned long int     uint32;
+	typedef signed long int       sint32;
+
+    typedef unsigned long long int uint64;
+	typedef signed long long int   sint64;
+	
+	typedef float                 float32;
+	typedef double                float64;
+	typedef long double           float128;
+	
+	typedef unsigned long long    uint132;
+	typedef signed long long      sint132;
+
+
+		
+#endif
